fb8c59e6bb1f5ff94d510fac1ea223c7733dbaed
ömer faruk.k <94250423+omerfarukk55@users.noreply.github.com>
2024-05-03 10:33:16 +0300

    update project
    son halin şekillendirdim mesafesini ekledim ne kadar uzaklıkta olduğu görecek kullanıcı

--- yolyardim.worktrees/master/src/Dashboard.tsx
+++ yolyardim.worktrees/master/src/Dashboard.tsx
@@ -1,12 +1,33 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Button, Keyboard } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { firebase } from '../config';
+
+const calculateDistance = async (origin, destination) => {
+  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&key=AIzaSyB3gmOSr3xGi3hAd-gfO5bTk5GXVwjk3TY`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.status === 'OK') {
+      const distance = data.rows[0].elements[0].distance.value; // Mesafe metre cinsinden
+      return distance / 1000; // Mesafeyi kilometreye dönüştür
+      
+    } else {
+      throw new Error('Mesafe hesaplanamadı');
+    }
+  } catch (error) {
+    console.error('Hata:', error);
+    throw error;
+  }
+};
 
 const Dashboard = (props) => {
   const [searchText, setSearchText] = useState("");
   const [results, setResults] = useState<any[]>([]);
+  const [selectedItem, setSelectedItem] = useState<any | null>(null);
   const map = useRef<MapView | null>(null);
   const [region, setRegion] = useState({
     latitude: 37.78825,
@@ -16,6 +37,7 @@ const Dashboard = (props) => {
   });
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [distance, setDistance] = useState(null);
 
   useEffect(() => {
     const requestLocationPermission = async () => {
@@ -70,7 +92,7 @@ const Dashboard = (props) => {
     const googleApisUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json";
     const input = searchText.trim();
     const location = `${region.latitude},${region.longitude}&radius=2000`;
-    const url = `${googleApisUrl}?query=${input}&location=${location}&key=apıkey`;
+    const url = `${googleApisUrl}?query=${input}&location=${location}&key=AIzaSyB3gmOSr3xGi3hAd-gfO5bTk5GXVwjk3TY`;
 
     try {
       const resp = await fetch(url);
@@ -98,7 +120,19 @@ const Dashboard = (props) => {
       console.log(e);
     }
   };
+  const handleMarkerPress = async (marker) => {
+    try {
+      const { coords } = location;
+      const origin = { latitude: coords.latitude, longitude: coords.longitude };
+      const destination = { latitude: marker.geometry.location.lat, longitude: marker.geometry.location.lng };
 
+      const distance = await calculateDistance(origin, destination);
+      setSelectedItem(marker);
+      setDistance(distance.toFixed(2));
+    } catch (error) {
+      console.error('Mesafe hesaplanamadı:', error);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.header}>{props.name}</Text>
@@ -113,17 +147,26 @@ const Dashboard = (props) => {
       >
         {results.length ? results.map((item, i) => (
           <Marker
-            key={`search-item-${i}`}
-            coordinate={{
-              latitude: item.geometry.location.lat,
-              longitude: item.geometry.location.lng
-            }}
-            title={item.name}
-            description="Description"
-          />
-        )) : null}
+          key={`search-item-${i}`}
+          coordinate={{
+            latitude: item.geometry.location.lat,
+            longitude: item.geometry.location.lng
+          }}
+          title={item.name}
+          onPress={() => handleMarkerPress(item)}>
+          </Marker>
+      )) : null}
       </MapView>
-
+      {selectedItem && (
+        <View >
+          <Callout>
+            <View style={styles.calloutContainer}>
+              <Text>{selectedItem.name}</Text>
+              <Text>Mesafe: {distance} km</Text>
+            </View>
+          </Callout>
+        </View>
+      )}
       <View style={styles.searchBox}>
         <Text>Arama Yap</Text>
         <TextInput
@@ -142,6 +185,20 @@ const Dashboard = (props) => {
 };
 
 const styles = StyleSheet.create({
+  calloutContainer:{
+   position: 'absolute',
+   bottom: 50,
+   fontSize: 20,
+   textAlign: 'center',
+   left: 50,
+   width: 150,
+   height: 70,
+   borderRadius: 10,
+   padding: 10,
+   shadowColor: '#000',
+   backgroundColor: '#fff',
+   zIndex:10,
+  },
   searchBox: {
     position: 'absolute',
     width: '80%',
